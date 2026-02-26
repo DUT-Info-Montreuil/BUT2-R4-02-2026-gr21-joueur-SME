c78287f63fd4a52770e9139cf1feee8c837e0bf7
Anas <anassemghouni774@gmail.com>
2026-02-26 12:17:01 +0100

    Initial commit

---
+++ src/main/java/universite_paris8/iut/qdev/tp202x/gr21/App.java
@@ -0,0 +1,10 @@
+package universite_paris8.iut.qdev.tp202x.gr21;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/universite_paris8/iut/qdev/tp202x/gr21/AppTest.java
@@ -0,0 +1,19 @@
+package universite_paris8.iut.qdev.tp202x.gr21;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
