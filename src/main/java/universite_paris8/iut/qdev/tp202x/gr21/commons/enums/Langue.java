6f57c475c76bcc6d319826fc2e8c947d75f84b28
Anas <anassemghouni774@gmail.com>
2026-03-12 11:21:16 +0100

    Modif des sur les enums

--- src/main/java/universite_paris8/iut/qdev/tp202x/gr21/commons/enums/Langue.java
+++ src/main/java/universite_paris8/iut/qdev/tp202x/gr21/commons/enums/Langue.java
@@ -4,11 +4,11 @@ public enum Langue {
 
     // null car pas encore réflechi
 
-    FRANCAIS(null, null, 0),
-    ANGLAIS(null, null, 0),
-    ALLEMAND(null, null, 0),
-    ESPAGNOL(null, null, 0),
-    ITALIEN(null, null, 0);
+    FRANCAIS("fr", "Francais", 1),
+    ANGLAIS("en", "Anglais", 2),
+    ALLEMAND("deu", "Allemand", 3),
+    ESPAGNOL("es", "Espagnol", 4),
+    ITALIEN("ita", "Italien", 5);
 
     private String libelleCourt;
     private String libelleLong;
